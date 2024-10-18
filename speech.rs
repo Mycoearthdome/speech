8c22a05a98dfd3406d95521bf09de170d9f07ec6
Mycoearthdome <122208982+Mycoearthdome@users.noreply.github.com>
2024-10-18 05:44:43 -0400

    Works better!

--- speech.rs
+++ speech.rs
@@ -2,7 +2,11 @@ extern crate cpal;
 extern crate vosk;
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use std::sync::{Arc, Mutex};
+use cpal::SampleFormat::I16;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 use vosk::{Model, Recognizer};
 
 const SAMPLE_RATE: f32 = 44100.0;
@@ -34,32 +38,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Ensure the sample rate is set to 16000
     input_config.sample_rate.0 = SAMPLE_RATE as u32;
     input_config.channels = 1;
-    input_config.buffer_size = cpal::BufferSize::Fixed(1024);
+    input_config.buffer_size = cpal::BufferSize::Fixed(44100 as u32);
 
     // Create the input stream once
-    let stream = input_device.build_input_stream(
+    let stream = input_device.build_input_stream_raw(
         &input_config,
+        I16,
         {
             let recognizer = Arc::clone(&recognizer);
-            move |data: &[f32], _| {
-                // Convert the f32 samples to i16 samples
-                let data: Vec<i16> = data.iter().map(|&x| (x * MAX_AMPLITUDE) as i16).collect();
+            move |data: &cpal::Data, _: &cpal::InputCallbackInfo| {
+                // Convert the data to a slice of i16 samples
+                let data: &[i16] = match data.sample_format() {
+                    cpal::SampleFormat::I16 => data.as_slice::<i16>().unwrap(),
+                    _ => panic!("Unexpected data format"),
+                };
 
                 // Feed the samples into the recognizer
                 let mut recognizer_lock = recognizer.lock().unwrap();
-                match recognizer_lock.accept_waveform(&data) {
+
+                match recognizer_lock.accept_waveform(data) {
                     vosk::DecodingState::Finalized => {
-                        let words = recognizer_lock.final_result().single().unwrap().text;
-                        let trimmed_words = words.trim_matches('"').to_string();
-                        if trimmed_words.len() > 0 {
+                        let words = recognizer_lock.final_result().single().unwrap();
+                        let trimmed_words = words.text.trim_matches('"').to_string();
+                        if !trimmed_words.is_empty() {
                             print!("{} ", trimmed_words);
                         }
                     }
-                    //vosk::DecodingState::Running => {
-                    //    let words = recognizer_lock.partial_result().partial;
-                    //    let trimmed_words = words.trim_matches('"').to_string();
-                    //    print!("{} ", trimmed_words);
-                    //}
                     _ => {}
                 }
             }
